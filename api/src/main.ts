1427c1acdfc826273076bd01f66b22e306795d90
MyThirdComputer <iguskov5995@gmail.com>
2026-06-08 20:50:29 +0300

    init project

---
+++ api/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
